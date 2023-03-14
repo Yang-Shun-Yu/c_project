135b8850aa87bddd15a7fc978675a66144a960b8
Yang-Shun-Yu <aebandafg@gmail.com>
2023-03-15 00:13:19 +0800

    B修改

--- test.c
+++ test.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 
+
 int main(){
     printf("hello");
     int x = 20;
     printf("the age is %d",x);
     printf("i did it\n");
-    printf("do something");
+    printf("Are you ok?");
     return 0;
 }
