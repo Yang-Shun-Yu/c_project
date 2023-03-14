5ec0699330baf9cd00074f74d68c8cd9be2821fc
Yang Shun <aebandafg@gmail.com>
2023-03-15 00:37:14 +0800

    修改

--- test.c
+++ test.c
@@ -2,10 +2,13 @@
 
 
 int main(){
-    printf("hello");
+    printf("hello\n");
     int x = 20;
-    printf("the age is %d",x);
+    printf("the age is %d\n",x);
     printf("i did it\n");
-    printf("Are you ok?");
+    int y;
+    printf("please input a number:");
+    scanf("%d",&y);
+    printf("the in put value is %d",y);
     return 0;
 }
